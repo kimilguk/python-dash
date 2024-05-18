f38ad5d72903484cb4220113c4ac020dacc25f71
kimilguk <boramcom@daum.net>
2024-05-18 14:01:32 +0900

    날자 형식 변경

--- pages/exchange_rate_app.py
+++ pages/exchange_rate_app.py
@@ -190,6 +190,9 @@ def update_output(my_dropdown_value):
         # df_exchange_rate2의 index를 날짜 열의 데이터로 변경
         df_exchange_rate2 = df_exchange_rate2.set_index('날짜')
         # df_exchange_rate2의 index를 datetime 형식으로 변환
+        print(df_exchange_rate2.index)
+        df_exchange_rate2.index = df_exchange_rate2.index.replace('-', '/')
+        print(df_exchange_rate2.index)
         df_exchange_rate2.index = pd.to_datetime(df_exchange_rate2.index,format='%Y-%m-%d')
         fig = px.line(df_exchange_rate2['매매기준율'], title="환율(매매기준율) 그래프", labels={"variable": "분류"}) # 단일 값 출력
         fig.update_layout(yaxis_title=f"원화/{my_dropdown_value}"
