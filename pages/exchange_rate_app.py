d59efce5328e4d2bb18eb2a8f7ec4a1600e4904d
kimilguk <boramcom@daum.net>
2024-05-18 15:11:12 +0900

    날짜 변환 확인

--- pages/exchange_rate_app.py
+++ pages/exchange_rate_app.py
@@ -59,9 +59,11 @@ df_exchange_rate2 = df_exchange_rate[::-1].reset_index(drop=True)
 df_exchange_rate2 = df_exchange_rate2.set_index('날짜')
 # df_exchange_rate2의 index를 datetime 형식으로 변환
 print('여기1',df_exchange_rate2.index)
+print('=====================')
 #df_exchange_rate2.index = df_exchange_rate2.index.replace('.', '-')
-print('여기2',df_exchange_rate2)
 df_exchange_rate2.index = pd.to_datetime(df_exchange_rate2.index,infer_datetime_format=True)
+print('여기2',df_exchange_rate2.index)
+print('=====================')
 # 1) 환율 데이터 표시
 # display(df_exchange_rate.tail())  # 환율 데이터 표시(앞의 일부만 표시)
 # display(df_exchange_rate2.head())  # 환율 데이터 표시(앞의 일부만 표시)
