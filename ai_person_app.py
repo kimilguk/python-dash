36852d67cf58cc206d5e539e5bf2cb196a6af8b7
kimilguk <boramcom@daum.net>
2024-05-24 17:53:07 +0900

    AI API 1개 이미지에서 사람의 속성 추출하기 기능 추가

--- ai_person_app.py
+++ ai_person_app.py
@@ -6,7 +6,7 @@ import base64
 # openApiURL = "http://aiopen.etri.re.kr:8000/ObjectDetect"
 openApiURL = "http://aiopen.etri.re.kr:8000/HumanParsing"
 accessKey = "cc902c9e-dd9b-4afa-bf83-3ffc585f81a2"
-imageFilePath = "senior-3336451_640.jpg"
+imageFilePath = "assets/senior-3336451_640.jpg"
 type = "jpg"
 #위 입력예시) ---------
 # https://pixabay.com/ko/photos/%EC%83%81%EC%9C%84-%EC%97%B0%EC%84%B8%EA%B0%80-%EB%93%9C%EC%8B%A0-%EC%82%AC%EB%9E%8C%EB%93%A4-3336451/
@@ -42,6 +42,31 @@ print(response.data) # print를 통해 결과가 json 형태로 나오는걸 보
 # 참고, 자바스크립트의 json과 파이썬의 딕셔너리는 같은 데이터 형이다.(아래)
 data = json.loads(response.data) # string 형태의 json이라서 json.loads를 통해 읽어서 딕셔너리로 반환한다.
 data_list = data.get("return_object") #딕셔너리에서 특정 값을 가져와서 딕셔너리로 반환한다.
+print(data_list)
 print('사진에 있는 사람의 수와 복장 특징 목록')
+
+# 데이터프레임 사용에 필요한 판다스 모듈 임포트
+import pandas as pd
+#####################################################################
+# dsah_table 모듈사용 시(아래) 
+# 데이터 프레임 생성
+df_person = pd.DataFrame(data_list) # 딕셔너리 데이터를 판다스 DataFrame으로 변환
+df_person = df_person.transpose() # 딕셔너리 데이터의 행열 변환
+# print(df_person.columns) # 컬럼출력
+df_person = df_person[['hair color','coat color','pants color']]
+df_person.columns = ['머리색상', '상의색상', '바지색상'] # 열 이름 변경
+df_person = df_person.reset_index().rename(columns={'index':'사람객체명'}) # 인덱스 명 출력
+print(df_person.head()) # 많은 데이터 중 상위 몇개만 표시
+#######################################################################
+
+#######################################################################
+# 데이터프레임.to_html()을 사용하여 웹용으로 변경 사용 시(아래)
+person_list = [] # 개체(json=dictionary) 배열변수 추가
 for i in data_list: # 하나씩 가져다가 리스트로 출력하는 코드입니다.
-    print(i,' = hair color:',data_list.get(i).get('hair color'),' coat color:',data_list.get(i).get('coat color'),' pants color:',data_list.get(i).get('pants color'))
+    person_list.extend([{'person':i,'hair color':data_list.get(i).get('hair color'),'coat color':data_list.get(i).get('coat color'),'pants color':data_list.get(i).get('pants color')}])
+    #print(i,' = hair color:',data_list.get(i).get('hair color'),' coat color:',data_list.get(i).get('coat color'),' pants color:',data_list.get(i).get('pants color'))
+# print(person_list)
+df_person_html = pd.DataFrame(person_list) # 딕셔너리 데이터를 판다스 DataFrame으로 변환
+# print(df_person.columns)
+df_person_html.columns = ['사람객체명', '머리색상', '상의색상', '바지색상'] # 열 이름 변경
+print(df_person_html.head()) # 많은 데이터 중 상위 몇개만 표시
--- pages/ai_person_app.py
+++ pages/ai_person_app.py
@@ -15,7 +15,7 @@ import base64
 # openApiURL = "http://aiopen.etri.re.kr:8000/ObjectDetect"
 openApiURL = "http://aiopen.etri.re.kr:8000/HumanParsing"
 accessKey = "cc902c9e-dd9b-4afa-bf83-3ffc585f81a2"
-imageFilePath = "senior-3336451_640.jpg"
+imageFilePath = "assets/senior-3336451_640.jpg"
 type = "jpg"
 #위 입력예시) ---------
 # https://pixabay.com/ko/photos/%EC%83%81%EC%9C%84-%EC%97%B0%EC%84%B8%EA%B0%80-%EB%93%9C%EC%8B%A0-%EC%82%AC%EB%9E%8C%EB%93%A4-3336451/
@@ -52,8 +52,39 @@ print(response.data) # print를 통해 결과가 json 형태로 나오는걸 보
 data = json.loads(response.data) # string 형태의 json이라서 json.loads를 통해 읽어서 딕셔너리로 반환한다.
 data_list = data.get("return_object") #딕셔너리에서 특정 값을 가져와서 딕셔너리로 반환한다.
 print('사진에 있는 사람의 수와 복장 특징 목록')
+# 데이터프레임 사용에 필요한 판다스 모듈 임포트
+import pandas as pd
+
+#####################################################################
+# dsah_table 모듈사용 시(아래) 
+# 데이터 프레임 생성
+df_person = pd.DataFrame(data_list) # 딕셔너리 데이터를 판다스 DataFrame으로 변환
+df_person = df_person.transpose() # 딕셔너리 데이터의 행열 변환
+# print(df_person.columns) # 컬럼출력
+df_person = df_person[['hair color','coat color','pants color']]
+df_person.columns = ['머리색상', '상의색상', '바지색상'] # 열 이름 변경
+df_person = df_person.reset_index().rename(columns={'index':'사람객체명'}) # 인덱스 명 출력
+print(df_person.head()) # 많은 데이터 중 상위 몇개만 표시
+#######################################################################
+
+#######################################################################
+# 데이터프레임.to_html()을 사용하여 웹용으로 변경 사용 시(아래)
+person_list = [] # 개체(json=dictionary) 배열변수 추가
 for i in data_list: # 하나씩 가져다가 리스트로 출력하는 코드입니다.
-    print(i,' = hair color:',data_list.get(i).get('hair color'),' coat color:',data_list.get(i).get('coat color'),' pants color:',data_list.get(i).get('pants color'))
+    person_list.extend([{
+    'person':i,
+    'hair color':'<div style="height:15px;width:15px;background-color:rgb'+data_list.get(i).get('hair color')+';display: inline-block;"></div>'+data_list.get(i).get('hair color'),
+    'coat color':'<div style="height:15px;width:15px;background-color:rgb'+data_list.get(i).get('coat color')+';display: inline-block;"></div>'+data_list.get(i).get('coat color'),
+    'pants color':'<div style="height:15px;width:15px;background-color:rgb'+data_list.get(i).get('pants color')+';display: inline-block;"></div>'+data_list.get(i).get('pants color')
+    }])
+    #print(i,' = hair color:',data_list.get(i).get('hair color'),' coat color:',data_list.get(i).get('coat color'),' pants color:',data_list.get(i).get('pants color'))
+# print(person_list)
+df_person_html = pd.DataFrame(person_list) # 딕셔너리 데이터를 판다스 DataFrame으로 변환
+# print(df_person.columns)
+df_person_html.columns = ['사람객체명', '머리색상', '상의색상', '바지색상'] # 열 이름 변경
+print(df_person_html.head()) # 많은 데이터 중 상위 몇개만 표시
+html_table = df_person_html.to_html(index=False, escape=False) # 데이터프레임을 문자열로 변경
+#####################################################################
 
 # 대시보드 앱 시작(아래)
 from dash import Dash, dcc, Input, Output, callback, callback_context, dash_table, State, html
@@ -61,13 +92,50 @@ import dash_bootstrap_components as dbc # 부트 스트랩 디자인 사용
 from dash.dash_table.Format import Format, Group # , type:'numeric', format:Format(group=True, groups=[4])
 from dash.exceptions import PreventUpdate
 
-app = Dash('s_search_app',external_stylesheets=[dbc.themes.BOOTSTRAP]) # __name__ 사용하지 않는 대신에 하단에 @callback 처럼 사용
+app = Dash('ai_person_app',external_stylesheets=[dbc.themes.BOOTSTRAP]) # __name__ 사용하지 않는 대신에 하단에 @callback 처럼 사용
 app.title = '사람속성 검출 AI를 사용한 웹 앱'
 layout = dbc.Container([ # 멀티 파일로 실행 할 때
 # app.layout = dbc.Container([ # 단일 파일로 실행 할 때
     html.H2(children='사람속성 검출 AI를 사용한 웹 앱'),
-    html.Div(children=data_list),
+    html.Div(children=[
+        html.Img(src=app.get_asset_url('senior-3336451_640.jpg'), width="300"),
+    ]),
+    dbc.Button("여러 이미지 검출 작업 중...", color="primary", className="me-1", id="btn_search6",
+               style={'display':'inline-block','width':'100px','vertical-align':'inherit'}),
+    dash_table.DataTable( # 스타일 참조: https://dash.plotly.com/datatable/style
+        columns=[
+        {'id': i, "name": i, 'presentation': 'markdown'} for i in df_person.columns], # , 'presentation': 'markdown'
+        id='tbl6',
+        data=df_person.to_dict("records"),
+        style_header={'textAlign': 'center'},
+        style_cell={'textAlign': 'left'},
+    ),
+    html.Div(children=[
+            dcc.Markdown(html_table, dangerously_allow_html=True)
+        ]),
     html.Div([
-        dcc.Loading(id="loading-1", type="circle", children=html.Div(id="loading-output-1")),
+        dcc.Loading(id="loading-6", type="circle", children=html.Div(id="loading-output-6")),
     ],style={'position':'fixed','left':'50%','top':'50%','z-index':'9999'}),
 ])
+@callback (
+    [Output('tbl6', 'data'),Output('errors6', 'children'),Output("loading-output-6", "children")],
+    [Input("btn_search6", "n_clicks")],
+    [State('tbl6', 'data'), State('tbl6', 'columns')], # 기존 값 가져오기
+#     prevent_initial_call=True, # 페이지 로드 시 콜백이 실행되지 않도록 
+)
+def update_table(value, n_clicks, data, columns):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0] # 클릭,상태변경 이벤트 확인용 변수
+    if 'btn_search6' in changed_id:
+        #new_value = value.replace(" ", "%20" )
+        #print(new_value)
+        try:
+            value=''
+            data=df_person.to_dict("records")
+        except:
+            value='검색결과를 파싱하지 못했습니다. 다른 검색어를 입력해 주세요.'
+            data=data
+        return data, value, ''
+
+# if __name__ == '__main__': #파이썬 파일이 메인 프로그램으로 사용될 때와 모듈로 사용될 때를 구분하기 위한 용도
+#     app.run_server(debug=False, host='0.0.0.0', port=8888) # 단일 파일로 실행 할 때
+# 데이터과학 기술참조: http://bigdata.dongguk.ac.kr/lectures/datascience/_book/index.html
